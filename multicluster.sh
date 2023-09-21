f8c7f927be9ea4b8c89219fa27b2ce5be44973f2
cpouthier <80251456+cpouthier@users.noreply.github.com>
2023-09-21 13:01:57 +0200

    Updated

--- multicluster.sh
+++ multicluster.sh
@@ -1,19 +1,49 @@
-
-echo -e "\033[0;101m Select the proper k10multicluster package before running/adapting this script visiting https://github.com/kastenhq/external-tools/releases\e[0m"
-
-wget https://github.com/kastenhq/external-tools/releases/download/6.0.7/k10multicluster_6.0.7_linux_amd64.tar.gz
-tar -xvf k10multicluster_6.0.7_linux_amd64.tar.gz 
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo -e "\033[0;101m Before running this script, visit https://github.com/kastenhq/external-tools/releases and find the appropriate k10multicluster package depending on your Kasten version, OS and architecture. \e[0m"
+echo -e "\033[0;101m Then, copy the k10multicluster package link in your clipboard as it will be requested by the script.\e[0m"
+echo -e "\033[0;101m \e[0m"
+echo -e "\033[0;101m For additional information, please check https://docs.kasten.io/latest/multicluster/bootstrap_reference.html#bootstrapping-commands-reference\e[0m"
+echo -e "\033[0;104m \e[0m"
+echo -e "\033[0;102m Paste the k10multicluster package URL and press Enter: \e[0m"
+read package_URL < /dev/tty
+echo "Downloading package..."
+wget $package_URL
+echo "Decompressing the package..."
+tar -xvf k10multicluster*.tar.gz 
 mv k10multicluster /usr/local/bin/k10multicluster
-rm k10multicluster_6.0.7_linux_amd64.tar.gz
+rm k10multicluster_*.tar.gz
+echo ""
+echo "Get contexts from kubeconfig..."
+echo ""
 kubectl config get-contexts
-echo "Which context is used for your primary cluster?"
-echo -e "\033[0;102m Enter the context name: \e[0m"
-read context_name < /dev/tty
-echo "What is the name of your primary cluster?"
-echo -e "\033[0;102m Enter the primary cluster name: \e[0m"
+echo ""
+echo ""
+echo -e "\033[0;102m Enter your primary cluster context name and press Enter: \e[0m"
+echo ""
+read primary_context_name < /dev/tty
+echo ""
+echo -e "\033[0;102m Enter your primary cluster name and press Enter: \e[0m"
+echo ""
 read primary_cluster_name < /dev/tty
-echo "What is the ingress path for your primary cluster?"
-echo -e "\033[0;102m Enter the ingress path URL: \e[0m"
-read ingress_path < /dev/tty
-k10multicluster  setup-primary --context=$context_name default --name=$primary_cluster_name --ingress=$ingress_path
+echo ""
+echo -e "\033[0;102m Enter your primary cluster ingress path URL and press Enter: \e[0m"
+echo ""
+read primary_ingress_path_url < /dev/tty
+echo ""
+curl -s https://github.com/cpouthier/kasten-scripts.git | bash
 
+
+#tests
+clear
+echo "valide avec ds"
+read testvar < /dev/tty
+if [ "$testvar" = ds ]; then
+        clear
+        echo "bien ouèj" 
+    else
+        clear
+        echo "pas de bol"
+fi
